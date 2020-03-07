cd9319c41300f8109976df63cf792d24184d1e79
Lin <sea952@yahoo.com.tw>
2020-03-08 02:15:41 +0900

    Add files via upload

---
+++ VB_Test/ExportCSV.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="Export to CSV" Language="vb" MasterPageFile="~/Site.Master" AutoEventWireup="false"
+    CodeBehind="ExportCSV.aspx.vb" Inherits="VB_Test.ExportCSV" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h2>
+        Test Export to CSV
+    </h2>
+    <p>
+        Click to Export CSV File
+    </p>
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+        <Columns>
+            <asp:TemplateField>
+                <EditItemTemplate>
+                    <asp:CheckBox ID="CheckBox1" runat="server" />
+                </EditItemTemplate>
+                <ItemTemplate>
+                    <asp:CheckBox ID="CheckBox1" runat="server" />
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Cust ID">
+                <ItemTemplate >
+                    <asp:Label ID = "custId" runat = "server" text = '<%# Container.DataItem %>' />
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+    </asp:GridView>
+    <br />
+    <asp:Button ID="Button1" runat="server" Text="Export" />
+    <br />
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+</asp:Content>
---
+++ VB_Test/ExportCSV.aspx.vb
@@ -0,0 +1,85 @@
+﻿Imports System.IO
+
+Public Class ExportCSV
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        If Not Me.IsPostBack Then
+            'Gridview資料
+            Dim arrCustID As String() = New String(3) {}
+            arrCustID(0) = "C01"
+            arrCustID(1) = "C02"
+            arrCustID(2) = "C03"
+            arrCustID(3) = "C04"
+            GridView1.DataSource = arrCustID
+            GridView1.DataBind()
+        End If
+
+
+    End Sub
+
+    Protected Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button1.Click
+
+        Dim CustNameArrayList As New ArrayList
+        With CustNameArrayList
+            .Add(New String() {0, "C01", "A"})
+            .Add(New String() {1, "C02", "B"})
+            .Add(New String() {2, "C03", "C"})
+            .Add(New String() {3, "C04", "D"})
+        End With
+
+        Dim selectedCust As String = ""
+        Dim selectedCustList As New ArrayList
+
+        'Checkbox取得的Data再去Database中取得資料
+        '這邊用Array代替,再改寫成從Database取得資料
+        For i As Integer = 0 To (GridView1.Rows.Count - 1)
+            Dim myCheckbox As CheckBox = GridView1.Rows(i).FindControl("CheckBox1")
+            Dim custName As Label = GridView1.Rows(i).FindControl("custId")
+
+            If myCheckbox.Checked = True Then
+                If String.IsNullOrEmpty(selectedCust) = True Then
+                    selectedCust = selectedCust
+                Else
+                    selectedCust = selectedCust & ", " & custName.Text
+                End If
+
+                For Each cust In CustNameArrayList
+                    If cust(1) = custName.Text Then
+                        selectedCustList.Add(New String() {cust(1), cust(2)})
+                    End If
+                Next
+            End If
+        Next
+
+        '確認Checkbox的資料用
+        Label1.Text = selectedCust
+
+        '匯出為csv檔
+
+        'CSVファイルに書き込むときに使うEncoding
+        Dim enc As System.Text.Encoding = System.Text.Encoding.GetEncoding("Shift_JIS")
+
+        '書き込むファイルを開く
+        Dim sr As New System.IO.StreamWriter("C:\CSV\Test.csv", False, enc)
+
+        'レコードを書き込む
+        For k As Integer = 0 To (selectedCustList.Count - 1)
+            'フィールドを書き込む
+            sr.Write(selectedCustList(k)(0).ToString())
+            'カンマを書き込む(依個人喜好調整)
+            sr.Write(","c)
+            'フィールドを書き込む
+            sr.Write(selectedCustList(k)(1).ToString())
+            'カンマを書き込む(依個人喜好調整)
+            sr.Write(","c)
+            '改行する
+            sr.Write(vbCrLf)
+        Next
+
+        '閉じる
+        sr.Close()
+
+    End Sub
+End Class
